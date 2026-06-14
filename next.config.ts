854aae8128fc301e8d932841dbe540a2fea00f56
Sahimc <53173388+Sahimc@users.noreply.github.com>
2026-06-15 00:35:36 +0100

    fix: suppress root hydration warning

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.prayertracker-ten.vercel.app',
-          },
-        ],
-        destination: 'https://prayertracker-ten.vercel.app/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
